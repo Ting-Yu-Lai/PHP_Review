a94caa80a5108365a18d5d1fdcc8c1da372bdc03
Akuma_Yu <manjusaka1110@gmail.com>
2025-05-04 18:07:16 +0800

    增加背景圖片，消除a tag底線 字體顏色白色

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,12 +13,13 @@
         justify-content: center;
 
     }
+
     li {
         padding: 10px 20px;
         border: 1px solid black;
-        border-radius:8px;
+        border-radius: 8px;
         box-shadow: 3px;
-        margin:10px 10px;
+        margin: 10px 10px;
 
     }
 
@@ -25,20 +27,46 @@
         background-color: black;
         padding: 15px 25px;
     }
-    a:hover {
+
+    a {
         color: white;
+        text-decoration: none;
+    }
+
+    .bg-container {
+        background-image: url('../image/bodypic.png');
+        background-size: cover;
+        /* background-position: center; 固定上面不裁掉王冠 */
+        background-repeat: no-repeat;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+    }
+
+    .overlay {
+        background-color: rgba(0, 0, 0, 0.5);
+
+        width: 100%;
+        height: 100%;
     }
 </style>
-<body>
-<div class="page_home">
-    <h1>PHP Review</h1>
 
-    <ul>
-        <li><a href="var.php">變數</a></li>
-        <li><a href="./array/">陣列設計</li>
-        <li><a href="./loop/index.php">迴圈設計</li>
-        <li><a href="./select/index.php">選擇結構</li>
-    </ul>   
-</div>
+<body>
+    <div class="bg-container">
+        <div class="overlay">
+            <h1>PHP Review</h1>
+
+            <ul>
+                <li><a href="var.php">變數</a></li>
+                <li><a href="./array/">陣列設計</li>
+                <li><a href="./loop/index.php">迴圈設計</li>
+                <li><a href="./select/index.php">選擇結構</li>
+            </ul>
+        </div>
+    </div>
 </body>
-</html>     
+
+</html>
